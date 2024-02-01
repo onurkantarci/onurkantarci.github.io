b3d20092220cbe55bed93c850b17964e46a99576
Onur Kantarci <onurkntrc@gmail.com>
2024-02-01 13:42:00 +0300

    feat: Create DescriptionLights for description

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,74 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import DescriptionLights from "./components/DescriptionLights.vue";
+</script>
 
 <template>
-  <div>Hello</div>
+  <div class="header">
+    <h1 class="name">Onur Kantarci</h1>
+    <h1 class="line">|</h1>
+    <h2 class="front-end">Frontend Developer</h2>
+  </div>
+  <div class="ceiling">
+    <DescriptionLights />
+  </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.line {
+  color: #ff5555; /* Reddish color */
+  position: relative;
+  animation: glitch 5s infinite linear;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+@keyframes glitch {
+  0%,
+  100% {
+    text-shadow: -2px -2px 2px #ff5555, 2px 2px 2px #ffffff;
+    color: #ff5555; /* Reddish color */
+  }
+  50% {
+    text-shadow: 2px -2px 2px #ff5555, -2px 2px 2px #ffffff;
+    color: #ffffff; /* Whiteish color */
+  }
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.header {
+  background: linear-gradient(
+    to right,
+    #232424,
+    #042642
+  ); /* Reddish to Blackish gradient */
+  padding: 10px;
+  background-color: transparent;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  gap: 10px;
+  max-height: 50px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
+}
+.name {
+  margin-left: 15px;
+  color: #ffffff; /* Whiteish color */
+  font-family: "Oswald", sans-serif;
+  font-weight: bold;
+  font-style: normal;
+  font-size: 25px;
+}
+.front-end {
+  color: #289dc4; /* Reddish color */
+  font-family: "Sixtyfour", sans-serif;
+  margin-top: 17px;
+  font-size: 15px;
+  font-weight: 400;
+  font-style: normal;
+}
+h3 {
+  color: #de3c4b; /* Reddish color */
+  font-family: "Sixtyfour", sans-serif;
+  font-size: 12px;
+}
+h4 {
+  color: #c42847; /* Reddish color */
+  font-family: "Roboto", sans-serif;
 }
 </style>
---
+++ src/components/DescriptionLights.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="lines-down">
+    <h1>|</h1>
+    <h1>|</h1>
+    <h1>|</h1>
+  </div>
+  <div
+    @click="turnOnLights"
+    :class="{ 'pulse-animation': lights }"
+    class="lines-left"
+  >
+    <h2>|</h2>
+    <h3>|</h3>
+    <h2>|</h2>
+    <h3>|</h3>
+    <h2>|</h2>
+  </div>
+  <transition name="fade">
+    <p
+      class="tip"
+      style="color: white; margin-left: 15px"
+      v-if="!lights"
+      key="paragraph1"
+    >
+      "Turning on a light is not just about dispelling darkness; it's an act of
+      unveiling possibilities and banishing uncertainty."
+    </p>
+  </transition>
+  <div v-if="lights" class="description">
+    <p>
+      Hey! I'm <span class="sixtyfour">ONUR</span>, a Frontend Developer from
+      <span class="sixtyfour">KOSOVO</span>.
+    </p>
+    <p>Currently I'm doing a lawyer internship in Ankara/Turkey,</p>
+    <p>which also I'm working on Frontend projects at the same time.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const lights = ref(false);
+
+const turnOnLights = () => {
+  lights.value = !lights.value;
+};
+</script>
+
+<style scoped>
+.tip {
+  position: absolute;
+  font-family: "Oswald", sans-serif;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+  opacity: 0;
+}
+.sixtyfour {
+  font-family: "Sixtyfour", sans-serif;
+}
+.description {
+  font-family: "Oswald", sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 40px;
+  flex-direction: column;
+  animation: pulsetext 1s infinite alternate;
+}
+
+@keyframes pulsetext {
+  0% {
+    color: rgba(255, 255, 255, 0.5);
+  }
+  100% {
+    color: rgb(255, 255, 255);
+  }
+}
+.lines-down {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.lines-left {
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  position: relative;
+}
+
+.pulse-animation {
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 100px;
+  height: 50px;
+  z-index: 1;
+  border-radius: 50%;
+  border: 1px solid rgb(27, 26, 26, 0.2);
+  animation: pulse 1s infinite alternate;
+}
+
+@keyframes pulse {
+  0% {
+    box-shadow: 10px 10px 10px rgba(40, 115, 196, 0.5);
+  }
+  100% {
+    box-shadow: 10px 10px 10px rgb(40, 102, 196);
+  }
+}
+
+h3 {
+  color: #3ca0de;
+  font-family: "Sixtyfour", sans-serif;
+  font-size: 12px;
+}
+
+h1,
+h2 {
+  color: #287ec4;
+  margin: 0;
+}
+</style>
