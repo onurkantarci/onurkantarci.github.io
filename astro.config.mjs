082c206987c3877836df72f0edb4d2ecb554e6f1
onurkantarci <onurkntrc@gmail.com>
2024-07-25 17:34:50 +0300

    Update Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://onurkantarci.github.io",
+  site: "https://onurkantarci.github.io/onurkantarci.github.io",
   base: "/onurkantarci.github.io/",
 });
